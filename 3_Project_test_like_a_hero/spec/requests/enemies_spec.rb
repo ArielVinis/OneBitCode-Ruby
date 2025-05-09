dde9736d5cdbf9c554478a0f959131344e3a5079
Ariel França <ariel.franca@hotmail.com>
2025-05-09 17:18:15 -0300

    Enhance enemy request specs with detailed error handling and delete functionality

--- 3_Project_test_like_a_hero/spec/requests/enemies_spec.rb
+++ 3_Project_test_like_a_hero/spec/requests/enemies_spec.rb
@@ -25,20 +25,51 @@ RSpec.describe "Enemies", type: :request do
         put "/enemies/#{enemy.id}", params: enemy_attributes
 
         json_response = JSON.parse(response.body)
-        expect(json_response).to eq(enemy.reload.as_json)
+        expect(enemy.reload.as_json).to eq(json_response)
       end
       
     end
 
     context "when the enemies does not exists" do
-      it "returns status code 404"
-      it "returns a not found message"
+      it "returns status code 404" do
+        put "/enemies/0", params: attributes_for(:enemy)
+
+        expect(response).to have_http_status(404)
+      end
+
+      it "returns a not found message" do
+        put "/enemies/0", params: attributes_for(:enemy)
+
+        expect(response.body).to match(/Enemy not found/)
+      end
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
+  describe "DELETE /enemies" do
+    context "when the enemy exists" do
+      it "returns http success" do
+        enemy = create(:enemy)
+        delete "/enemies/#{enemy.id}"
+        expect(response).to have_http_status(204)
+      end
+
+      it "destroy the record" do
+        enemy = create(:enemy)
+        delete "/enemies/#{enemy.id}"
+        expect{enemy.reload}.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+    
+    context "when the enemy does not exists" do
+      it "returns status code 404" do
+        delete "/enemies/0"
+        expect(response).to have_http_status(404)
+      end
+
+      it "returns a not found message" do
+        delete "/enemies/0"
+        expect(response.body).to match(/Enemy not found/)
+      end
     end
   end
-
 end
